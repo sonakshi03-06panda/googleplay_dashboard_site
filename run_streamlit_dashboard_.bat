f5145ebeff4c87ae10b4c118e8f02bedb7382e9a
Sonakshi <2201730005@krmu.edu.in>
2025-07-21 12:31:14 +0530

    deploy

--- run_streamlit_dashboard_.bat
+++ run_streamlit_dashboard_.bat
@@ -1,5 +1,4 @@
 @echo off
 cd /d "%~dp0"
-C:\Users\Sonakshi\AppData\Local\Programs\Python\Python313\python.exe -m streamlit run "C:\Users\Sonakshi\OneDrive\Attachments\Desktop\googleplay_dashboard_site\scripts\streamlit_app.py"
-
+C:\Users\Sonakshi\AppData\Local\Programs\Python\Python313\python.exe -m streamlit run "C:\Users\Sonakshi\OneDrive\Attachments\Desktop\PROJECTS_apps\googleplay_dashboard_site\scripts\streamlit_app.py"
 pause
