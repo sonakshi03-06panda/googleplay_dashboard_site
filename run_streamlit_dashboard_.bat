0d48e688d25edebd6f293ad004ecb2cb1b82b8b9
Sonakshi <2201730005@krmu.edu.in>
2025-07-21 12:32:44 +0530

    delete

--- run_streamlit_dashboard_.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "%~dp0"
-C:\Users\Sonakshi\AppData\Local\Programs\Python\Python313\python.exe -m streamlit run "C:\Users\Sonakshi\OneDrive\Attachments\Desktop\PROJECTS_apps\googleplay_dashboard_site\scripts\streamlit_app.py"
-pause
